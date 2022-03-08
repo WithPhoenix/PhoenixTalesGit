628e33df16f4d29f05673bfd637c74dc9fbb8f8d
WithPhoenix <felix.unger1@outlook.de>
2022-03-08 13:30:19 +0100

    added energy holder resources and the class

--- src/main/java/com/phoenix/phoenixtales/rise/item/items/EnergyHolder.java
+++ src/main/java/com/phoenix/phoenixtales/rise/item/items/EnergyHolder.java
@@ -1,24 +1,58 @@
 package com.phoenix.phoenixtales.rise.item.items;
 
+import com.phoenix.phoenixtales.core.creativetab.ItemTab;
 import com.phoenix.phoenixtales.rise.RiseItem;
 import net.minecraft.block.BlockState;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
+import net.minecraftforge.energy.IEnergyStorage;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+//should the energy transfer be handled in the Block class or this item class
+//if the block has a special interface, the energy could just be transferred, knowing the block can handle energy
+//how do you get the target block?
+//what is smarter?
 
 public class EnergyHolder extends RiseItem {
-    public EnergyHolder(Properties properties) {
-        super(properties);
+
+    private boolean autocharge;
+    private int energy;
+
+    public EnergyHolder() {
+        super(new Properties().group(ItemTab.ITEM_GROUP).maxStackSize(1));
+        this.autocharge = false;
+        this.energy = 0;
+    }
+
+
+    @Override
+    public int getRGBDurabilityForDisplay(ItemStack stack) {
+        return MathHelper.hsvToRGB(50, 100, 100);
+//        return MathHelper.rgb(255, 214, 0);
     }
 
     @Override
-    public boolean updateItemStackNBT(CompoundNBT nbt) {
-        return super.updateItemStackNBT(nbt);
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        if (stack.getTag().getBoolean("autocharge")) {
+            tooltip.add(new TranslationTextComponent("tooltip.phoenixtales.energyholder.autocharge.on"));
+        }else {
+            tooltip.add(new TranslationTextComponent("tooltip.phoenixtales.energyholder.autocharge.off"));
+        }
+        String energyString = this.energy + " kJ";
+        tooltip.add(new StringTextComponent(energyString));
+        super.addInformation(stack, worldIn, tooltip, flagIn);
     }
-
 
     @Override
     public boolean canPlayerBreakBlockWhileHolding(BlockState state, World worldIn, BlockPos pos, PlayerEntity player) {
@@ -26,8 +60,13 @@ public class EnergyHolder extends RiseItem {
     }
 
     @Override
-    public int getRGBDurabilityForDisplay(ItemStack stack) {
-        return super.getRGBDurabilityForDisplay(stack);
+    public boolean updateItemStackNBT(CompoundNBT nbt) {
+        return super.updateItemStackNBT(nbt);
+    }
+
+    @Override
+    public boolean hasEffect(ItemStack stack) {
+        return stack.getTag().getBoolean("autocharge");
     }
 
     @Override
