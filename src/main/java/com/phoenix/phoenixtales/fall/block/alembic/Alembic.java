26df39c1505b233c1af2d954e0e34f1d5a15d9e6
WithPhoenix <felix.unger1@outlook.de>
2022-05-09 16:46:37 +0200

    updated or added

---
+++ src/main/java/com/phoenix/phoenixtales/fall/block/alembic/Alembic.java
@@ -0,0 +1,13 @@
+package com.phoenix.phoenixtales.fall.block.alembic;
+
+import net.minecraft.block.Block;
+
+public class Alembic extends Block {
+
+
+    public Alembic(Properties properties) {
+        super(properties);
+    }
+
+
+}
