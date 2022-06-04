e1951e2321c9c933463cc23e62beb6ab86ee966c
WithPhoenix <felix.unger1@outlook.de>
2022-06-04 10:29:54 +0200

    updated or added

--- src/main/java/com/phoenix/phoenixtales/origins/world/feature/ov/Ores.java
+++ src/main/java/com/phoenix/phoenixtales/origins/world/feature/ov/Ores.java
@@ -4,10 +4,10 @@ import com.phoenix.phoenixtales.rise.block.RiseBlocks;
 import net.minecraft.block.Block;
 
 public enum Ores {
-    OVERWORLD_NICKEL(RiseBlocks.ORE_NICKEL, 3, 0, 70, 3),
-    OVERWORLD_VANADIUM(RiseBlocks.ORE_VANADIUM, 3, 0, 70, 2),
-    OVERWORLD_KERNITE(RiseBlocks.ORE_KERNITE, 4, 40, 90, 2),
-    OVERWORLD_APATITE(RiseBlocks.ORE_APATITE, 4, 40, 90, 2),
+    OVERWORLD_NICKEL(RiseBlocks.ORE_NICKEL, 3, 0, 60, 4),
+    OVERWORLD_VANADIUM(RiseBlocks.ORE_VANADIUM, 3, 0, 60, 4),
+    OVERWORLD_KERNITE(RiseBlocks.ORE_KERNITE, 3, 40, 90, 2),
+    OVERWORLD_APATITE(RiseBlocks.ORE_APATITE, 3, 40, 90, 2),
     OVERWORLD_TIN(RiseBlocks.ORE_TIN, 4, 0, 80, 4),
     OVERWORLD_COPPER(RiseBlocks.ORE_COPPER, 4, 10, 90, 4);
 
